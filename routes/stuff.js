d41c4f01b3142eb1cc91007dc788357aaba44e91
moukmessie <moukmessie@gmail.com>
2022-02-14 23:49:17 +0100

    system login and signin updated

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,39 +1,12 @@
 const express = require('express');
 const router =express.Router();
-const Thing = require('../models/Thing');
+const controllerStuff=require('../controllers/stuff');
 
-//add-object
-router.post('/',(req,res, next)=>{
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(()=>res.status(201).json({message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({error}));
-});
-//update one object
-router.put('/:id',(req, res,next)=>{
-    Thing.updateOne({_id: req.params.id}, {...req.body,_id: req.params.id})
-        .then(()=>res.status(200).json({message:'Objet modifié !'}))
-        .catch(error=>res.status(400).json({error}));
-});
-//Delete One object
-router.delete('/:id',(req,res,next)=>{
-    Thing.deleteOne({_id: req.params.id})
-        .then(()=>res.status(200).json({message:'Objet supprimé'}))
-        .catch(error=>res.status(400).json({error}));
-});
-//get one objet
-router.get('/:id', (req,res, next)=>{
-    Thing.findOne({_id: req.params.id})
-        .then(thing=>res.status(200).json(thing))
-        .catch(error=>res.status(404).json('Objet non trouvé !'))
-});
-//get all objects
-router.get('/',(req,res,next)=>{
-    Thing.find()
-        .then(things=>res.status(200).json(things))
-        .catch(error=>res.status(400).json({error}));
-});
+router.get('/',controllerStuff.getAllThings);
+router.post('/',controllerStuff.createThing);
+router.put('/:id',controllerStuff.modifyThing);
+router.delete('/:id',controllerStuff.deleteThings);
+router.get('/:id', controllerStuff.getOneThing);
+
+
 module.exports = router;
